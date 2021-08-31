298b0c58b95cf4186386e8831f3ebd6f0ea2881b
Udani Imalka <uimalka.96@gmail.com>
2021-08-31 15:31:36 +0530

    Testing style component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 3em;
+  text-align:center;
+  color:palevioletred;
+`;
+
+const Wrapper = styled.section`
+  padding:4em;
+  background:papayawhip;
+`;
 
 function App() {
   return (
-    <h1 style={{textAlign:"center"}}>Hello</h1>
+    <Wrapper>
+      <Title>
+      <h1 style={{textAlign:"center"}}>Hello</h1>
+      </Title>
+    </Wrapper>
+    
   );
 }
 
