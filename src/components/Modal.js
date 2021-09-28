83e6ad5f909e441a2fe21abacf41b343979347a1
Udani Imalka <uimalka.96@gmail.com>
2021-09-28 17:03:00 +0530

    add style width

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -16,10 +16,10 @@ export const DialogBox = styled.div`
   background: antiquewhite;
   width: 33%;
   height: auto;
-  top: "50%";
-  left: "50%";
-  transform: "50%";
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   border-radius: 10px;
-  padding: 075rem;
+  padding: 0.75rem;
   color: rgba(0, 0, 139, 0.7);
 `;
--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -5,7 +5,8 @@ export const TabsContainer = styled.div`
   overflow: hidden;
   background: #fff;
   height: 100%;
-  width: 100%;
+  max-width: 80%;
+  min-width: 80%;
 `;
 
 const TabButtonContainer = styled.div`
