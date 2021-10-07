89bde129c6126ab36e8ff336a901fe83515b5603
Udani Imalka <uimalka.96@gmail.com>
2021-10-07 11:42:05 +0530

    Add put request util,and add burrow book API

--- src/api/bookAPI.js
+++ src/api/bookAPI.js
@@ -1,7 +1,10 @@
-import { getRequest } from "./util";
+import { getRequest, putRequest } from "./util";
 
 const BASE_URL = "/book";
 
 export const getBooks = () => getRequest(`${BASE_URL}`);
 
 export const getBook = (id) => getRequest(`${BASE_URL}/${id}`);
+
+export const leadBook = (id, burrowedMemberId, burrowedDate) => 
+    putRequest(`${BASE_URL}/${id}/burrow`, {burrowedMemberId, burrowedDate});
--- src/api/util.js
+++ src/api/util.js
@@ -16,3 +16,19 @@ export const getRequest = async (uri) => {
     };
   }
 };
+
+export const putRequest = async (uri, data) => {
+  try {
+    let response = await axios.put(uri, data);
+
+    return {
+      data: response.data,
+      error: null
+    };
+  } catch (error) {
+    return {
+      data: null,
+      error: error
+    };
+  }
+}
