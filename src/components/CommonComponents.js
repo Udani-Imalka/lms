6e63d34e9bbf1d303d1ec5f9505b6a742c8683c6
Udani-Imalka <55935208+Udani-Imalka@users.noreply.github.com>
2021-10-08 22:13:33 +0530

    Update CommonComponents.js

--- src/components/CommonComponents.js
+++ src/components/CommonComponents.js
@@ -5,7 +5,66 @@ export const FluidContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  `;
 
+export const Container = styled.div`
+  display: flex;
+  flex-direction: ${(props) => props.flexDirection ? props.flexDirection : "column"};
+  justify-content: ${(props) => props.justifyContent ? props.justifyContent : "center"};
+  align-items: ${(props) => props.alignItems ? props.alignItems : "flex-start"};
+  width: ${(props) => props.disableFullWidth ? "initial": "100%"};
+  padding: 1em;
+`;
+
+export const ContainerInline = styled.div`
+  flex: 1 1 0;
+  display: inline-flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 1em;
+  text-align: left;
+`;
+
+export const FlexRow = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 1em 5em;
+`;
+
+export const Button = styled.button`
+  background-color: ${(props) => {
+    switch (props.color) {
+      case "danger":
+        return props.theme.primary.danger;
+      case "secondary":
+        return props.theme.secondary.main;
+      default:
+        return props.theme.primary.main;
+    }
+  }};
+  color: ${(props) => props.theme.primary.textColor};
+  font-size: ${(props) => (props.size ? props.size : 1)}em;
+  padding: 0.25em 1em;
+  border: 0;
+  border-radius: 0.5em;
+  cursor: pointer;
+  margin: 1em;
+
+$ {(props) => props.rounded ? "border-radius: 50%; padding: 0.5em;": ""}
+
+  :hover {
+    background-color: ${(props) => {
+      switch (props.color) {
+        case "danger":
+          return props.theme.primary.danger;
+        case "secondary":
+          return props.theme.secondary.dark;
+        default:
+          return props.theme.primary.dark;
+      }
+    }};
 `;
 
 export const Select = styled.select`
