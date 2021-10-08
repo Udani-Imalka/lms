a238b9795cb7af281f8be8559000bd36b44d087b
Udani Imalka <uimalka.96@gmail.com>
2021-10-08 22:06:02 +0530

    Add clear input

--- src/containers/Dashboard/Books/AddBookDialog.js
+++ src/containers/Dashboard/Books/AddBookDialog.js
@@ -15,9 +15,15 @@ export default function AddBookDialog ({handleClose, show}){
     const [title,setTitle] =useState("");
     const [author,setAuthor] = useState("");
 
+    const clearInputs = ()=> {
+        setTitle("")
+        setAuthor("")
+    }
     const sendDone = () => {
         if(title !== "" && author !== "") {
-            handleClose(true, {title,author});
+            const data = { title,author}
+            clearInputs();
+            handleClose(true, data);
         } else if (title === "") {
             window.alert("Please enter a title to add.");
         } else {
@@ -26,7 +32,10 @@ export default function AddBookDialog ({handleClose, show}){
         
     };
 
-    const sendCancel = () => handleClose(false,null);
+    const sendCancel = () => {
+        clearInputs();
+        handleClose(false,null);
+    };
 
     return (
         <Modal show={show}>
--- src/containers/Dashboard/Books/Book.js
+++ src/containers/Dashboard/Books/Book.js
@@ -145,7 +145,7 @@ const Book = ({ id, handleBackClick }) => {
         handleClose={handleReturn}
         show={showReturnConfirmation}
         headerText="Confirm book return"
-        detailText="Press 'Confirm' to return book"
+        detailText="Press 'Yes'to  confirm return book"
       />
     </>
   );
