94ce93ed3ac2e3de91c818a1d1e4d3aa08f67dc5
Udani Imalka <uimalka.96@gmail.com>
2021-10-10 09:16:44 +0530

    use redux action in dashboard

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,22 +1,27 @@
 import React, { useEffect, useState } from "react";
+import { useSelector,useDispatch } from "react-redux";
 
 import Tabs from "../../components/Tabs";
 import Spinner from "../../components/Spinner";
 
 import Books from "./Books/index";
 
+import { setBooks } from "../../store/booksSlice";
 import { getBooks } from "../../api/bookAPI";
 
 const Dashboard = () => {
   const [isLoading, setIslaoding] = useState(false);
-  const [books, setBooks] = useState(null);
+ 
+  const booksFromRedux = useSelector((state) => state.books.value);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     setIslaoding(true);
     getBooks()
       .then((response) => {
         if (!response.error) {
-          setBooks(response.data);
+          console.log(response.data);
+          dispatch(setBooks(response.data));
         }
       })
       .catch((error) => {
@@ -25,10 +30,10 @@ const Dashboard = () => {
       .finally(() => {
         setIslaoding(false);
       });
-  }, []);
+  }, [dispatch]);
 
   const contents = [
-    { title: "Books", elements: <Books catalog={books} /> },
+    { title: "Books", elements: <Books catalog={booksFromRedux} /> },
     { title: "Members", elements: <h1>Contents of memebers go here</h1> },
   ];
 
