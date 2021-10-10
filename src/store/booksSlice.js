ef4af45ab14c8ab0041be8de03da08dd5ff7667d
Udani Imalka <uimalka.96@gmail.com>
2021-10-10 11:45:50 +0530

    Add add and delete funtionalities

--- src/store/booksSlice.js
+++ src/store/booksSlice.js
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+
 export const booksSlice = createSlice({
     name: 'books',
     initialState: {
@@ -18,9 +19,23 @@ export const booksSlice = createSlice({
         updatedBooks.splice(index, 1,action.payload);
         state.value = updatedBooks;
       },
+      addBook: (state,action) => {
+        const updatedBooks = [...state.value];
+        updatedBooks.push(action.payload);
+        state.value = updatedBooks;
+      },
+      deleteBook: (state,action) => {
+        const updatedBooks = [...state.value];
+        const id = action.payload;
+        const index= updatedBooks.findIndex(
+          (element) => element.id === id
+        );
+        updatedBooks.splice(index, 1);
+        state.value = updatedBooks;
+      },
     },
   })
 
-  export const { setBooks, updateBook } = booksSlice.actions;
+  export const { setBooks, updateBook , addBook, deleteBook } = booksSlice.actions;
 
 export default booksSlice.reducer;
